673aac84496df462d43b0390c1727a674b309c52
jbivy <bivens.james@gmail.com>
2021-04-13 14:24:18 -0600

    clients and server 4/13

--- src/client2.java
+++ src/client2.java
@@ -3,14 +3,14 @@ import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 
-public class client {
+public class client2 {
     public PrintWriter socketWriter;
     public BufferedReader socketReader;
     public BufferedReader userReader;
     public String coordinates;
     public int turn = 1;
     public static void main(String[] args){
-        (new client()).go();
+        (new client2()).go();
     }
 
     private void go() {
@@ -21,16 +21,23 @@ public class client {
             socketReader = new BufferedReader(new InputStreamReader(sock.getInputStream()));
             userReader = new BufferedReader(new InputStreamReader(System.in));
             while((coordinates = userReader.readLine()) != null){
-                if(turn == 1) {
+                /*if(turn == 2) {
                     socketWriter.println(coordinates);
                     socketWriter.flush();
-                    turn = 2;
-                }
-                if(turn == 2){
-                    String response = socketReader.readLine();
-                    System.out.println("Response from player 2:" + response);
                     turn = 1;
                 }
+                if (turn == 1){
+                    String response = socketReader.readLine();
+                    System.out.println("Response from player 1:" + response);
+                    turn = 2;
+                }
+
+                 */
+                String response = socketReader.readLine();
+                System.out.println("Response from player 1:" + response);
+
+                socketWriter.println(coordinates);
+                socketWriter.flush();
             }
 
         } catch (Exception exception) {
