61aace1f71a43c9644fcdd405490736b62615ab8
Matt Barich <matthew.barich@western.edu>
2021-05-04 11:27:53 -0600

    Updated Grid

--- src/Grid.java
+++ src/Grid.java
@@ -15,6 +15,20 @@ public class Grid {
         return grid;
     }
 
+    private void place_user_ships(int x_ship_starting_position, int y_ship_starting_position){
+        boolean valid_ship_placement = false;
+        while(!valid_ship_placement){
+            for(int row = 0; row<NUM_ROW;row++){
+                for(int col = 0; col<NUM_COL;col++){
+                    if(row == x_ship_starting_position && col == y_ship_starting_position && grid[row-1][col-1] != "1"){
+                        grid[row-1][col-1] = "1";
+                        valid_ship_placement = true;
+                    }
+                }
+            }
+        }
+    }
+
     void print_grid() {
         for (String[] strings : grid) {
             for (int col = 0; col < grid.length; col++) {
@@ -24,31 +38,25 @@ public class Grid {
         }
     }
 
-    void place_ship() {
-        int x_ship_starting_position = (int) (Math.random() * 3 - 1) + 1;
-        int y_ship_starting_position = (int) (Math.random() * 3 - 1) + 1;
-        for (int row = 0; row < NUM_ROW; row++) {
-            for (int col = 0; col < NUM_COL; col++) {
-                if (row == x_ship_starting_position && col == y_ship_starting_position) {
-                    grid[row - 1][col - 1] = "1";
-                }
-            }
+    void place_ship(){
+        for(int i=0; i < 3; i++){
+            int x_ship_starting_position = (int) (Math.random()*5 - 1) + 1;
+            int y_ship_starting_position = (int) (Math.random()*5 - 1) + 1;
+            place_user_ships(x_ship_starting_position, y_ship_starting_position);
         }
     }
 
-    String check_player_guess(String returnVal) {
+    String check_player_guess(String returnVal){
         line = returnVal.trim().split(",");
         int user_x_coordinate = Integer.parseInt(line[0]);
         int user_y_coordinate = Integer.parseInt(line[1]);
-        if (grid[user_x_coordinate - 1][user_y_coordinate - 1] == "1") {
+        if(grid[user_x_coordinate-1][user_y_coordinate-1] == "1") {
             System.out.println("HIT");
-            grid[user_x_coordinate - 1][user_y_coordinate - 1] = "H";
+            grid[user_x_coordinate-1][user_y_coordinate-1] = "H";
             returnVal = "hit";
-        } else {
+        }else{
             returnVal = "miss";
         }
         return returnVal;
     }
-
-
 }
